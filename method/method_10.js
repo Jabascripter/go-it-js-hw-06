adfb1f313cbabd25c64130091935f4d655b44ef1
Yura <56349630+Jabascripter@users.noreply.github.com>
2020-05-25 14:53:30 +0300

    Add files via upload

---
+++ method/method_10.js
@@ -0,0 +1,13 @@
+import users from "../users.js";
+
+const getSortedUniqueSkills = (users) => {
+  return users
+    .reduce((allSkills, user) => {
+      allSkills.push(...user.skills);
+      return [...new Set(allSkills)];
+    }, [])
+    .sort();
+};
+
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
